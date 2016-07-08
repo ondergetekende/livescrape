b767ea8e888db6440ce83ed15ed6c646515d7cbd
Koert van der Veer <koert@ondergetekende.nl>
2016-07-08 17:05:55 +0200

    Prepare for 0.9.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='livescrape',
-    version='0.9.7',
+    version='0.9.8',
     url='https://github.com/ondergetekende/python-livescrape',
     description='A toolkit to build pythonic web scraper libraries',
     author='Koert van der Veer',
